63f9e22be3e0deca9d8c34e50c049942f2813f03
Iván Salazar <ivansalazar@kybel.mx>
2020-06-16 16:16:38 -0500

    Change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='dhl_delivery',
-    version='2.0.9',
+    version='2.0.10',
     author='Iván Salazar',
     author_email='ivangio.salazar@gmail.com',
     packages=['dhl_delivery'],
